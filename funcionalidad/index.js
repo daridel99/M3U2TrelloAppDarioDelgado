d66e552a6c857f41bcf90af9cdbfbc4315c04738
Dario Alberto Delgado Portillo <89899431+daridel99@users.noreply.github.com>
2022-07-13 02:30:05 -0500

    API PUT y API DELETE
    Se hace uso de la funcion con la que se usa el click para pasar las tareas de una columna a otra para obtener la columna de destino donde se define que el estado de la tarea es 1, 2 o 3 siendo "Por Hacer", "Haciendo" y "Terminada" respectivamente, para actualizar dicho estado en la API y asi finalizar con la funcion PUT.
    Se tiene la funcion GET para optener los datos de la API y ponerlos en la Trello app, se tiene la funcion POST para enviar los datos nuevos creados desde la Trello App, se tiene la funcion DELETE con el proposito de eliminar alguna tarea despues de una confirmacion y por ultimo la funcion PUT para actializar el estado al mover de columna.

--- funcionalidad/index.js
+++ funcionalidad/index.js
@@ -1,5 +1,8 @@
 
 var cont=1;
+const listaActividadesUI1 = document.getElementById('.example-origin');
+const reload = document.querySelector('#reload');
+
 function crear(){
 	
 	
@@ -10,35 +13,35 @@ function crear(){
 	var radioV = parseInt(document.querySelector('input[type=radio][name=language]:checked').value);
 	var dateControl = document.querySelector('input[type="date"]').value;
 	var usuarioT = document.getElementById('result');
-	console.log(dateControl)
+	//console.log(dateControl)
 	switch(radioV){
 
 		case 1:
 
 		var valor = document.getElementById('example-origin1');
-		document.getElementById('example-origin1').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ "</div>";
+		document.getElementById('example-origin1').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\"confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+cont+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel')\"><i class=\"material-icons\">delete</i></button></span>  </div>";
 		//document.getElementById('result').innerHTML="";
 		//document.getElementById('Contenido').disabled = true;
 		radio.checked = false;
-		cont+=1;
+		//cont+=1;
 		break;
 
 		case 2:
 
 		var valor = document.getElementById('example-origin2');
-		document.getElementById('example-origin2').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad+ '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ "</div>";
+		document.getElementById('example-origin2').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\"confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+cont+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel')\"><i class=\"material-icons\">delete</i></button></span>  </div>";
 		//document.getElementById('Contenido').disabled = true;
 		radio.checked = false;
-		cont+=1;
+		//cont+=1;
 		break;
 
 		case 3:
 
 		var valor = document.getElementById('example-origin3');
-		document.getElementById('example-origin3').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad+ '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ "</div>";
+		document.getElementById('example-origin3').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\"confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+cont+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel')\"><i class=\"material-icons\">delete</i></button></span>  </div>";
 		//document.getElementById('Contenido').disabled = true;
 		radio.checked = false;
-		cont+=1;
+		//cont+=1;
 		break;
 
 	}
@@ -71,6 +74,10 @@ function crear(){
 
 
 		})
+	
+			cont+=1;
+			console.log(cont)
+	
 			}
 
 
@@ -91,14 +98,43 @@ function onDragOver(event) {
   event.preventDefault();
 }
 
+/* API PUT*/
+
 function onDrop(event) {
+
+    nombre=event.target.id;
+
+    if(event.path[0].className=='example-dropzone'){
+      esdoC=3;
+    }
+    else if(event.path[0].className=='example-origin'){
+      esdoC=1;
+    }
+    else{
+    	esdoC=2;
+    }
+
     const id = event
     .dataTransfer
     .getData('text');
     const draggableElement = document.getElementById(id);
+    idC=id.toString().slice(10);
+    	
+    	fetch('https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/'+idC,{
+	method: 'PUT',
+	headers:{
+
+				'Content-Type': 'application/json',
+
+			},
+
+			body:JSON.stringify({
+
+				estado: esdoC })})
+
     const dropzone = event.target;
     dropzone.appendChild(draggableElement);
-
+    console.log("estado "+esdoC+", id "+idC)
     event
 
       .dataTransfer
