d53939f472a361fe7a08fb7ebc54a1da714133fa
daridel99 <darialber@gmail.com>
2022-07-30 16:22:59 -0500

    se aneÃxa libreria de AXIOS y se eliminan comentarios de code no usados

--- funcionalidad/index.js
+++ funcionalidad/index.js
@@ -12,36 +12,27 @@ function crear(){
 	var radioV = parseInt(document.querySelector('input[type=radio][name=language]:checked').value);
 	var dateControl = document.querySelector('input[type="date"]').value;
 	var usuarioT = document.getElementById('result');
-	//console.log(dateControl)
 	switch(radioV){
 
 		case 1:
 
 		var valor = document.getElementById('example-origin1');
 		document.getElementById('example-origin1').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\"confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+cont+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel')\"><i class=\"material-icons\">delete</i></button></span>  </div>";
-		//document.getElementById('result').innerHTML="";
-		//document.getElementById('Contenido').disabled = true;
 		radio.checked = false;
-		//cont+=1;
 		break;
 
 		case 2:
 
 		var valor = document.getElementById('example-origin2');
 		document.getElementById('example-origin2').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\"confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+cont+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel')\"><i class=\"material-icons\">delete</i></button></span>  </div>";
-		//document.getElementById('Contenido').disabled = true;
 		radio.checked = false;
-		//cont+=1;
 		break;
 
 		case 3:
 
 		var valor = document.getElementById('example-origin3');
 		document.getElementById('example-origin3').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuarioT.innerHTML + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\"confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+cont+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel')\"><i class=\"material-icons\">delete</i></button></span>  </div>";
-		//fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+cont+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove())
-		//document.getElementById('Contenido').disabled = true;
 		radio.checked = false;
-		//cont+=1;
 		break;
 
 	}
@@ -49,26 +40,23 @@ function crear(){
 		
 		/* API POST */
 
-		fetch('https://62c7cf638c90491c2ca7c0e9.mockapi.io/works',{
-			method: 'POST',
-			headers:{
+		axios.post('https://62c7cf638c90491c2ca7c0e9.mockapi.io/works', {
 
-				'Content-Type': 'application/json',
+			id: cont,
+			usuario: usuarioT.innerHTML,
+			fecha: dateControl,
+			estado: radioV,
+			tarea: actividad
 
-			},
+		  })
 
-			body:JSON.stringify({
+		  .then(function (response) {
+			console.log(response);
+		  })
 
-				id: cont,
-				usuario: usuarioT.innerHTML,
-				fecha: dateControl,
-				estado: radioV,
-				tarea: actividad
-
-			})
-
-
-		})
+		  .catch(function (error) {
+			console.log(error);
+		  });
 
 		cont+=1;
 	
@@ -88,17 +76,38 @@ function onDragStart(event) {
     event
     .currentTarget
     .style
-    /*.backgroundColor = 'yellow';*/
+
 }
+
+function onDragleave(event) {
+	event.preventDefault();
+  
+	event
+	  .currentTarget
+	  .style
+	  .backgroundColor = '';
+  
+  }
 
 function onDragOver(event) {
   event.preventDefault();
+
+  event
+    .currentTarget
+    .style
+    .backgroundColor = '#ffed4b';
+
 }
 
 function onDrop(event) {
 
-		nombre=event.target.id;
-    //console.log(nombre.toString())
+	nombre=event.target.id;
+
+	event
+    .currentTarget
+    .style
+    .backgroundColor = '';
+
 if(event.path[0].className=='example-dropzone' || event.path[0].className=='example-origin' || event.path[0].className=='origin-dropzone'){
     if(event.path[0].className=='example-dropzone'){
       esdoC=3;
@@ -157,35 +166,29 @@ if(event.path[0].className=='example-dropzone' || event.path[0].className=='exam
 	var dateControl = pelicula.fecha;
 	var usuario = pelicula.usuario;
 	var idT= pelicula.id;
-		cont=idT;
-	//console.log(dateControl)
+	cont=idT;
 
 	switch(radioV){
 
 		case 1:
 
 		var valor = document.getElementById('example-origin1');
-		document.getElementById('example-origin1').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuario + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <span class=\"float-right\"><button type=\"button\" onclick=\" confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+idT+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel') \" ><i class=\"material-icons\">delete</i></button></span> </div>";
-		//cont+=1;
+		document.getElementById('example-origin1').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuario + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\" confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+idT+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel') \" ><i class=\"material-icons\">delete</i></button></span> </div>";
 		break;
 
 		case 2:
 
 		var valor = document.getElementById('example-origin2');
-		document.getElementById('example-origin2').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuario + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <span class=\"float-right\"><button type=\"button\" onclick=\" confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+idT+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel') \" ><i class=\"material-icons\">delete</i></button></span> </div>";
-		//cont+=1;
+		document.getElementById('example-origin2').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuario + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\" confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+idT+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel') \" ><i class=\"material-icons\">delete</i></button></span> </div>";
 		break;
 
 		case 3:
 
 		var valor = document.getElementById('example-origin3');
-		document.getElementById('example-origin3').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuario + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <span class=\"float-right\"><button type=\"button\" onclick=\" confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+idT+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel') \" ><i class=\"material-icons\">delete</i></button></span></div>";
-		//cont+=1;
+		document.getElementById('example-origin3').innerHTML= valor.innerHTML + " <div id=\"draggable-"+cont+"\" class=\"example-draggable\" draggable=\"true\" ondragstart= \" onDragStart(event); \" >" + "Usuario encargado: <center> <br> <b>" + usuario + '</b> </center> <br>'+ "Tarea: " +actividad + '<br>'+ "<center> <br> Fecha: <br> </center>" +dateControl+ " <br> <br> <span class=\"float-right\"><button type=\"button\" onclick=\" confirm('confirma que desa borrarlo?') ? fetch( 'https://62c7cf638c90491c2ca7c0e9.mockapi.io/works/"+idT+"',{method: 'DELETE',}).then(() => $('#draggable-"+cont+"').remove()) : console.log('You clic cancel') \" ><i class=\"material-icons\">delete</i></button></span></div>";
 		break;
 
 	}
-
-	//console.log(cont);
 		
     });
 
